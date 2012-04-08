724ffb07fc1905ecde27edec474f3b148d64ef3d
Richard Kwant <rkwant@gmail.com>
2012-04-08 13:58:09 -0700

    finished the getData method in politics.py and added support to electionprediction for selecting the race and for plotting

--- HW5/politics.py
+++ HW5/politics.py
@@ -1,16 +1,27 @@
 import sqlite3
+import time
 
 def foo(var):
 	print 'hello'
 
-def getData(name, date, dbloc):
+def getData(name, date, race, dbloc):
 
 	#connect to the database, change this to re-create the database.
 	connection = sqlite3.connect(dbloc)
 	cursor = connection.cursor()
 	
-	query = "SELECT * FROM candidates WHERE name = ?"
-	cursor.execute(query, [name])
+	a=time.strptime(date, '%Y-%m-%d')
+	nDate = time.strftime('%b %d, %Y',a).replace(' 0', ' ')
 	
+	query = "SELECT * FROM predictions WHERE candidate LIKE ? AND race = ? AND date = ?"
+	cursor.execute(query, ["%"+name+"%", race, nDate])
+	predictionResults = cursor.fetchall()
 	
-	print 'it all worked'
+	i=-1
+	for i,point in enumerate(predictionResults):
+		print 'The closing value for',point[3],'on',date,'was',point[1]
+	if i == -1:
+		print 'There were no results foound for the given date'
+		
+	
+	connection.close()
--- HW8/ElectionPredictions.py
+++ HW8/ElectionPredictions.py
@@ -11,15 +11,21 @@ parser.add_argument('-c', action='store', dest='candidate',
                     help='Candidate Name')
 parser.add_argument('-d', action='store', dest='date',
                     help='What is the date?')
+parser.add_argument('-r', action='store', default='2012 Presidential Election', dest='race',
+                    help='What is the race?')
 parser.add_argument('-p', action='store_true', default=False,
                     dest='plot',
                     help='Plot the results?')
-					
+									
 parser.add_argument('--version', action='version', version='%(prog)s 1.0')
 results = parser.parse_args()
 
 candidate = results.candidate
 date = results.date
 toPlot = results.plot
+race = results.race
 
-getData(candidate, date, 'db/politics.db')
+if toPlot:
+	getData(candidate, date, race, 'db/politics.db')
+else:
+	getData(candidate, date, race, 'db/politics.db')
