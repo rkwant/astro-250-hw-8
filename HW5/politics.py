6468dcaa074040ee612b6f98cc2715fae9b749db
Richard Kwant <rkwant@gmail.com>
2012-04-08 13:31:12 -0700

    added getData to politics, and argparse code to electionpredictions

--- HW5/politics.py
+++ HW5/politics.py
@@ -1,2 +1,16 @@
+import sqlite3
+
 def foo(var):
 	print 'hello'
+
+def getData(name, date, dbloc):
+
+	#connect to the database, change this to re-create the database.
+	connection = sqlite3.connect(dbloc)
+	cursor = connection.cursor()
+	
+	query = "SELECT * FROM candidates WHERE name = ?"
+	cursor.execute(query, [name])
+	
+	
+	print 'it all worked'
--- HW8/ElectionPredictions.py
+++ HW8/ElectionPredictions.py
@@ -1,4 +1,25 @@
 import sys
+import argparse
+sys.path.append("..")
 
-sys.path.append("../HW5")
+from HW5.politics import foo
+from HW5.politics import getData
 
+parser = argparse.ArgumentParser(description='Show election prediction data')
+
+parser.add_argument('-c', action='store', dest='candidate',
+                    help='Candidate Name')
+parser.add_argument('-d', action='store', dest='date',
+                    help='What is the date?')
+parser.add_argument('-p', action='store_true', default=False,
+                    dest='plot',
+                    help='Plot the results?')
+					
+parser.add_argument('--version', action='version', version='%(prog)s 1.0')
+results = parser.parse_args()
+
+candidate = results.candidate
+date = results.date
+toPlot = results.plot
+
+getData(candidate, date, 'db/politics.db')
